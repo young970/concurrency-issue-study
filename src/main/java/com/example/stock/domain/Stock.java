d2a12aeaea35ad25b6f5242270e45c4cbdc65354
young970 <yyww9@naver.com>
2023-10-10 18:14:46 +0900

    init

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,39 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("foo");
+        }
+
+        this.quantity -= quantity;
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+}
---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,41 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setup() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void tearDown(){
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void 재고감소(){
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+}
