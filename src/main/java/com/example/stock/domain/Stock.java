089afe586e2c83026cc1b354b212b7a575b6bbd6
young970 <yyww9@naver.com>
2023-10-11 01:53:01 +0900

    Feat: OptimisticLock으로 해결하는 방법 추가

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 
 @Entity
 public class Stock {
@@ -15,6 +16,9 @@ public class Stock {
     private Long productId;
 
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock() {
     }
